98e1e7889cac13f1a543bf21dccf714c6bc19831
lostboy529 <44226731+lostboy529@users.noreply.github.com>
2019-12-16 21:23:47 +0530

    Create account.php

---
+++ account.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+$Name = $_POST['username'];
+date_default_timezone_set('Asia/Calcutta');
+$now = date('Y-m-d H:i:s', time());
+?>
+<html>
+<head>
+</head>
+<body>
+</body>
+</html>
